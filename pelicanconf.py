4fb561f462027292259088a05cc454a4b49d23a8
jpic <jamespic@gmail.com>
2016-05-23 13:07:33 +0200

    Fallback on local theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+import os
+
 AUTHOR = u'Memopol Team'
 SITENAME = u'Memopol Project'
 SITEURL = 'http://www.memopol.org/'
@@ -56,6 +58,9 @@ PLUGINS = ['pin_to_top']
 
 #Theme
 THEME = "/var/www/memopol-blog/foundation-memopol"
+
+if not os.path.exists('THEME'):
+    THEME = 'foundation-memopol'
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
